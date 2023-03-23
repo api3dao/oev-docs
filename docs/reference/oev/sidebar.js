e9e56eb73d17d80eda03e4fe93e167d06d15d1e5
doge_bull <69164627+jgcogsystematictrading@users.noreply.github.com>
2023-03-23 09:10:59 -0700

    Update sidebar.js

--- docs/reference/oev/sidebar.js
+++ docs/reference/oev/sidebar.js
@@ -3,6 +3,10 @@ module.exports = [
     text: 'OEV Relay',
     items: [
       {
+        text: '• OEV Relay Overview',
+        link: '/reference/oev/Overview/',
+      },
+      {
         text: '• API Endpoints',
         link: '/reference/oev/endpoints/',
       },
